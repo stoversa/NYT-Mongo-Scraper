20bf5b6d48f104fdf30c94ef23f8b9b9c58e6165
stoversa <stoversa@gmail.com>
2018-04-09 20:27:11 -0400

    Added handlebars

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ var cheerio = require("cheerio"); //Scraper
 // Require all models
 var db = require("./models");
 
+// Port configuration for local/Heroku
 const PORT = process.env.PORT || process.argv[2] || 8080;
 
 // Initialize Express
@@ -17,6 +18,11 @@ var app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 // Use express.static to serve the public folder as a static directory
 app.use(express.static("public"));
+
+// Handlebars
+const exphbs = require("express-handlebars");
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
 
 // Connect to the Mongo DB
 var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
@@ -30,5 +36,5 @@ mongoose.connect(MONGODB_URI, {
 
 // Start the server
 app.listen(PORT, function () {
-    console.log("App running on port " + PORT + "!");
+    console.log(`This application is running on port: ${PORT}`);
 });
