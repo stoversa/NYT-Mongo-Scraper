908a02347a46dc8d71f5597dd0924a642c156391
stoversa <stoversa@gmail.com>
2018-04-14 16:16:11 -0400

    updated server

--- server.js
+++ server.js
@@ -14,19 +14,19 @@ const PORT = process.env.PORT || process.argv[2] || 8080;
 // Initialize Express
 const app = express();
 
-
 // Use body-parser for handling form submissions
 app.use(bodyParser.urlencoded({ extended: true }));
-// Use express.static to serve the public folder as a static directory
-app.use(express.static("public"));
 
 // Handlebars
 const exphbs = require("express-handlebars");
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
+// Use express.static to serve the public folder as a static directory
+app.use(express.static("public"));
 // Controllers
-require("./controllers/api")(app);
+const router = require("./controllers/api.js");
+app.use(router);
 // Connect to the Mongo DB
 const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
 
