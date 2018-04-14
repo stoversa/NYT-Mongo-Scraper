aa1ac810731abde3cfd7327dacbbaa8f62d57f49
stoversa <stoversa@gmail.com>
2018-04-14 16:45:42 -0400

    Adding routes for saving articles/retrieving all saved articles

--- controllers/api.js
+++ controllers/api.js
@@ -83,6 +83,24 @@ router.get("/", (req, res) => {
         });
 });
 
+router.get("/saved", (req, res) => {
+    db.Article.find({isSaved: true})
+        .then(function (retrievedArticles) {
+            // If we were able to successfully find Articles, send them back to the client
+            let hbsObject;
+            hbsObject = {
+                articles: retrievedArticles
+            };
+            res.render("saved", hbsObject);
+            console.log("The first thing ran!!")
+            
+        })
+        .catch(function (err) {
+            // If an error occurred, send it to the client
+            res.json(err);
+        });
+});
+
 // Route for getting all Articles from the db
 router.get("/articles", function (req, res) {
     // Grab every document in the Articles collection
@@ -95,7 +113,11 @@ router.get("/articles", function (req, res) {
             // If an error occurred, send it to the client
             res.json(err);
         });
-}),
+});
+
+router.put("/save/:id", function (req, res) {
+    db.Article.findOneAndUpdate({ _id: req.params.id }, { isSaved: true }, { new: false });
+});
 
 // Route for grabbing a specific Article by id, populate it with it's note
 router.get("/articles/:id", function (req, res) {
