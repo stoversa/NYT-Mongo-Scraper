6f4ea2cbd63220a0f6a86224163e4ca78737a490
stoversa <stoversa@gmail.com>
2018-04-14 22:46:58 -0400

    working on public js

--- public/assets/js/public.js
+++ public/assets/js/public.js
@@ -1,21 +1,60 @@
 $(document).ready(function () {
     // event handler for deleting a note
-    $(".delete-btn").click((event)=> {
+    $(".delete-btn").click(function (event) {
         event.preventDefault();
-        //delete route
+        const id = $(this).attr("data");
+        $.ajax(`/remove/${id}`, {
+            type: "PUT"
+        }).then(function(){
+            location.reload();
+        })
+    });
+    
+    $('.delete-note').click(function (event) {
+        event.preventDefault();
+        const id = $(this).attr("data");
+        $.ajax(`/note/${id}`, {
+            type: "DELETE"
+        }).then(function () {
+            location.reload();
+        });
+        $('#note-modal').modal('toggle');
     });
 
     // event handler for opening the note modal
-    $(".note-btn").click((event) => {
+    $(".note-btn").click(function (event) {
         event.preventDefault();
-        const noteID = $(this).attr("data");
-        console.log(noteID);
-        //post route
+        const id = $(this).attr("data");
+        $('#article-id').text(id);
+        $('#save-note').attr('data', id);
+        $.ajax(`/articles/${id}`, {
+            type: "GET"
+        }).then(function (data) {
+            $('.articles-available').empty();
+            if (data[0].note.length > 0){
+                data[0].note.forEach(v => {
+                    $('.articles-available').append($(`<li class='list-group-item'>${v.text}<button type='button' class='btn btn-danger btn-sm float-right delete-note' data='${v._id}'>X</button></li>`));
+                })
+            }
+            else {
+                $('.articles-available').append($(`<li class='list-group-item'>No notes for this article yet</li>`));
+                console.log("Second ran!")
+            }
+        })
+        $('#note-modal').modal('toggle');
     });
 
-    $("#save-note").click((event) => {
+    $("#save-note").click(function (event) {
         event.preventDefault();
-        const noteText = $('#"note-input').val().trim();
+        const id = $(this).attr('data');
+        const noteText = $('#note-input').val().trim();
+        $('#note-input').val('');
+        $.ajax(`/note/${id}`, {
+            type: "POST",
+            data: { text: noteText}
+        }).then(function (data) {
+            console.log(data)
+        })
         $('#note-modal').modal('toggle');
     });
 
