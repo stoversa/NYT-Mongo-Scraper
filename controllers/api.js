a8e32e2e8b0f2422d8bc48a5c377c5bc4afe76b2
stoversa <stoversa@gmail.com>
2018-04-15 09:46:17 -0400

    Resolved multiple note display issue

--- controllers/api.js
+++ controllers/api.js
@@ -43,13 +43,12 @@ router.get("/scrape", (req, res) => {
                     db.Article.create(result)
                         .then(function (dbArticle) {
                             // View the added result in the console
-                            console.log(dbArticle);
+                            count++;
                         })
                         .catch(function (err) {
                             // If an error occurred, send it to the client
                             return res.json(err);
                         });
-                    count++;
                 };
             });
             // If we were able to successfully scrape and save an Article, redirect to index
@@ -137,7 +136,10 @@ router.get("/articles/:id", function (req, res) {
     // Using the id passed in the id parameter, prepare a query that finds the matching one in our db...
     db.Article.find({ _id: req.params.id })
         // ..and populate all of the notes associated with it
-        .populate("note")
+        .populate({
+            path: 'note',
+            model: 'Note'
+        })
         .then(function (dbArticle) {
             // If we were able to successfully find an Article with the given id, send it back to the client
             res.json(dbArticle);
@@ -156,7 +158,7 @@ router.post("/note/:id", function (req, res) {
             // If a Note was created successfully, find one Article with an `_id` equal to `req.params.id`. Update the Article to be associated with the new Note
             // { new: true } tells the query that we want it to return the updated User -- it returns the original by default
             // Since our mongoose query returns a promise, we can chain another `.then` which receives the result of the query
-            return db.Article.findOneAndUpdate({ _id: req.params.id }, { note: dbNote._id }, { new: true });
+            return db.Article.findOneAndUpdate({ _id: req.params.id }, {$push: { note: dbNote._id }}, { new: true });
         })
         .then(function (dbArticle) {
             // If we were able to successfully update an Article, send it back to the client
